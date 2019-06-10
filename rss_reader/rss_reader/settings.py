dc0341cb4f64419565169b9fb7274b54d572d9ea
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-06-09 23:13:09 -0700

    Updated settings

--- rss_reader/rss_reader/settings.py
+++ rss_reader/rss_reader/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rss'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'rss_reader.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': 'RSS',
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('PG_PASS'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
 
